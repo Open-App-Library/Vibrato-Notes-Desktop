a6c9d6c0ebf12168f17ee9295e9bce551f010463
Doug Beney <contact@dougie.io>
2019-01-04 12:19:53 -0500

    moved qmake to the end

--- gen_compile_commands.sh
+++ gen_compile_commands.sh
@@ -3,8 +3,6 @@
 mkdir -p qmake
 cd qmake
 
-qmake -project "QT += testlib" ../VibratoNotes-desktop.pro
-
 if [ $1 == "help" ] || [ $1 == "h" ] || [ $1 == "-h" ] || [ $1 == "--help" ]; then
     echo "gen_compile_commands.sh"
     echo "======================="
@@ -24,4 +22,5 @@ elif [ $1 == "clean" ]; then
     exit
 fi
 
+qmake -project "QT += testlib" ../VibratoNotes-desktop.pro
 bear -a -o ../compile_commands.json make
