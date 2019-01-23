68dd695d30a166439ec0de4c31042bedad9fb115
Doug Beney <contact@dougie.io>
2019-01-23 18:06:42 -0500

    Tag Database

--- src/meta/db/tagdatabase.cpp
+++ src/meta/db/tagdatabase.cpp
@@ -16,32 +16,6 @@ QVector<Tag *> TagDatabase::list() const
   return m_list;
 }
 
-int TagDatabase::getUniqueTagID(int start, QVector<Tag*> tagList, Tag *tagToSync)
- {
-  // Loop through tag list. If ID == start (The current index we're testing)
-  // that means that the ID is NOT unique and we must find a new one. Increment
-  // by one!
-  for ( Tag* t : tagList )
-    if ( t->id() == start ) // Can't use this ID; it's taken.
-      return getUniqueTagID(start+1, tagList);
-
-  // If we made it this far in the function, it means that the ID we are testing
-  // is indeed unique. Return that bad boy.
-  return start;
-}
-
-int TagDatabase::getUniqueTagID(Tag *tagToSync)
-{
-  // Search for a unique tag ID.
-  // If you have tag ID #1 and create a new tag, the next ID will
-  // most likely be two. For this reason, we set the start index to search
-  // for a new ID to the list length + 1 as it is likely unused.
-  //   Tip: If the *tagToSync parameter is passed, and the user has
-  //        connected the cloud. It will attempt to sync to the cloud first.
-  QVector<Tag*> tagList = list();
-  return getUniqueTagID(tagList.length()+1, tagList, tagToSync);
-}
-
 int TagDatabase::size() const
 {
   return m_list.size();
@@ -56,25 +30,25 @@ void TagDatabase::addTag(Tag *tag)
   }
 
   m_list.append(tag);
-  connect(tag, &Tag::tagChanged,
-          this, &TagDatabase::tagChanged_slot);
+  connect(tag, &Tag::changed,
+          this, &TagDatabase::changed_slot);
 
-  emit tagAdded(tag);
+  emit added(tag);
 }
 
 Tag *TagDatabase::addTag(QString title)
 {
-  QString titleCleaned = title.trimmed();
-  if ( title.isEmpty() )
-    return nullptr;
+  // Clean tag & make sure not empty string
+  title = title.trimmed();
+  if ( title.isEmpty() ) return nullptr;
 
   // Check if tag exists. Return if so.
-  Tag *existingTag = findTagWithName(titleCleaned);
+  Tag *existingTag = findTagWithName(title);
   if ( existingTag ) return existingTag;
 
-  Tag *tag = new Tag(-1, -1, title);
-  m_sqlManager->addTag(tag); // Will fetch proper ID.
-
+  Tag *tag = new Tag();
+  tag->setTitle(title);
+  m_sqlManager->addTag(tag);
   addTag(tag);
 
   return tag;
@@ -83,13 +57,13 @@ Tag *TagDatabase::addTag(QString title)
 void TagDatabase::removeTag(int index)
 {
   Tag *tag = m_list[index];
-  int id = tag->id();
+  QUuid sync_hash = tag->syncHash();
 
   m_sqlManager->deleteTag(tag);
   delete tag;
 
   m_list.removeAt(index);
-  emit tagRemoved(id);
+  emit removed(sync_hash);
 }
 
 void TagDatabase::removeTag(Tag *tag)
@@ -108,10 +82,10 @@ void TagDatabase::clearTags()
   }
 }
 
-Tag *TagDatabase::findTagWithID(int id)
+Tag *TagDatabase::findTagWithSyncHash(QUuid syncHash)
 {
   for (Tag *tag : m_list) {
-    if (tag->id() == id)
+    if (tag->syncHash() == syncHash)
       return tag;
   }
   return nullptr;
@@ -125,15 +99,6 @@ Tag *TagDatabase::findTagWithName(QString name)
   return nullptr;
 }
 
-// Tag *TagDatabase::findTagWithNameOrCreate(QString name)
-// {
-//   Tag *tag = findTagWithName( name );
-//   if (tag != nullptr)
-//     return tag;
-//   else
-//     return addTag(name);
-// }
-
 void TagDatabase::loadSQL()
 {
   QVector<Tag*> tags = m_sqlManager->tags();
@@ -141,28 +106,8 @@ void TagDatabase::loadSQL()
     addTag(tag);
 }
 
-void TagDatabase::loadJSON(QJsonDocument jsonDocument)
-{
-  QJsonArray tagArray = jsonDocument.array();
-  for (int i = 0; i < tagArray.size(); i++) {
-    QJsonObject curTag = tagArray[i].toObject();
-    int tag_sync_id = curTag.value("sync_id").toInt();
-    int tag_id = curTag.value("id").toInt();
-    QString tag_title = curTag.value("title").toString();
-    Tag *t = new Tag(tag_sync_id, tag_id, tag_title);
-    addTag(t);
-  }
-}
-
-void TagDatabase::loadDummyTags()
-{
-  clearTags();
-  QJsonDocument Tags = HelperIO::fileToQJsonDocument(":/dummy/tags.json");
-  loadJSON(Tags);
-}
-
-void TagDatabase::tagChanged_slot(Tag *tag)
+void TagDatabase::changed_slot(Tag *tag)
 {
   m_sqlManager->updateTagToDB(tag);
-  emit tagChanged(tag);
+  emit changed(tag);
 }
--- src/meta/db/tagdatabase.h
+++ src/meta/db/tagdatabase.h
@@ -3,6 +3,7 @@
 #include <QObject>
 #include <QVector>
 #include <QJsonDocument>
+#include <QUuid>
 #include "../tag.h"
 #include "../../sql/sqlmanager.h"
 
@@ -12,37 +13,27 @@ class TagDatabase : public QObject
 public:
   TagDatabase(SQLManager *sqlManager);
   QVector<Tag*> list() const;
-  int getUniqueTagID(int start, QVector<Tag*> tagList, Tag *tagToSync=nullptr);
-  int getUniqueTagID(Tag *tagToSync=nullptr);
   int           size() const;
 
   void addTag(Tag *tag);
-  // These addTag functions with a 'title' argument will first check if there is already
-  // an existing tag object availible with the same string as 'title'. (case-insensitive,
-  // leading and trailing whitespace removed.)
   Tag *addTag(QString title);
 
   void removeTag(int index);
   void removeTag(Tag *tag);
   void clearTags();
 
-  Tag *findTagWithID(int id);
+  Tag *findTagWithSyncHash(QUuid syncHash);
   Tag *findTagWithName(QString name);
-  // *DEPRECATED*
-  // Tag *findTagWithNameOrCreate(QString name);
 
   void loadSQL();
 
-  void loadJSON(QJsonDocument jsonDocument);
-  void loadDummyTags();
-
 private slots:
-  void tagChanged_slot(Tag *tag);
+  void changed_slot(Tag *tag);
 
 signals:
-  void tagAdded(Tag *tag);
-  void tagRemoved(int tagID);
-  void tagChanged(Tag *tag);
+  void added(Tag *tag);
+  void changed(Tag *tag);
+  void removed(QUuid tagSyncHash);
 
 private:
   SQLManager *m_sqlManager;
