8eaf2a1e7a1ba287aa627eeeafd2ed5e0de2f385
Doug Beney <contact@dougie.io>
2019-01-26 13:39:17 -0500

    Cleaner excerpts

--- src/models/delegates/noteitemdelegate.cpp
+++ src/models/delegates/noteitemdelegate.cpp
@@ -7,115 +7,116 @@
 #include "../items/notelistitem.h"
 
 NoteItemDelegate::NoteItemDelegate(QListView *view, QSortFilterProxyModel *proxyModel) :
-    m_view(view),
-    m_proxyModel(proxyModel)
+  m_view(view),
+  m_proxyModel(proxyModel)
 {
 
 }
 
 QRect NoteItemDelegate::getStarRect(const QStyleOptionViewItem &option) const
 {
-    return QRect(option.rect.x()+option.rect.width()-35,
-                 option.rect.y()+option.rect.height()/2-12,
-                 25,25);
+  return QRect(option.rect.x()+option.rect.width()-35,
+               option.rect.y()+option.rect.height()/2-12,
+               25,25);
 }
 
 
 bool NoteItemDelegate::editorEvent(QEvent *event,
-                             QAbstractItemModel *model,
-                             const QStyleOptionViewItem &option,
-                             const QModelIndex &index)
+                                   QAbstractItemModel *model,
+                                   const QStyleOptionViewItem &option,
+                                   const QModelIndex &index)
 {
-    if (event->type() == QEvent::MouseButtonRelease)
+  if (event->type() == QEvent::MouseButtonRelease)
     {
-        QMouseEvent* mouseEvent = static_cast<QMouseEvent*>(event);
-        if(mouseEvent->button() == Qt::LeftButton)
+      QMouseEvent* mouseEvent = static_cast<QMouseEvent*>(event);
+      if(mouseEvent->button() == Qt::LeftButton)
         {
-            QRect checkboxRect = getStarRect(option);
-            QPoint mousePoint = mouseEvent->pos();
-            if ( checkboxRect.contains(mousePoint) ) {
-                QModelIndex realIndex = m_proxyModel->mapToSource(index);
-                NoteListItem *item = static_cast<NoteListItem*>( realIndex.internalPointer() );
-                item->note()->setFavorited( !item->note()->favorited() );
-                return true;
-            }
-            else
-                m_view->setCurrentIndex(index);
+          QRect checkboxRect = getStarRect(option);
+          QPoint mousePoint = mouseEvent->pos();
+          if ( checkboxRect.contains(mousePoint) ) {
+            QModelIndex realIndex = m_proxyModel->mapToSource(index);
+            NoteListItem *item = static_cast<NoteListItem*>( realIndex.internalPointer() );
+            item->note()->setFavorited( !item->note()->favorited() );
+            return true;
+          }
+          else
+            m_view->setCurrentIndex(index);
         }
     }
 
-    return QStyledItemDelegate::editorEvent(event, model, option, index);
+  return QStyledItemDelegate::editorEvent(event, model, option, index);
 }
 
 void NoteItemDelegate::paint(QPainter *painter, const QStyleOptionViewItem &option, const QModelIndex &index) const
 {
-    if (index.column() == 0 ) {
-        QModelIndex realIndex = m_proxyModel->mapToSource(index);
-        NoteListItem *item = static_cast<NoteListItem*>( realIndex.internalPointer() );
+  if (index.column() == 0 ) {
+    QModelIndex realIndex = m_proxyModel->mapToSource(index);
+    NoteListItem *item = static_cast<NoteListItem*>( realIndex.internalPointer() );
 
-        // Set the background color
-        QBrush background =
-                (option.state & QStyle::State_Selected) ?
-                    option.palette.highlight() :
-                    option.palette.base();
-        painter->fillRect(option.rect, background);
+    // Set the background color
+    QBrush background =
+      (option.state & QStyle::State_Selected) ?
+      option.palette.highlight() :
+      option.palette.base();
+    painter->fillRect(option.rect, background);
 
-        if (option.state & QStyle::State_Selected)
-            painter->setPen(option.palette.highlightedText().color());
-        else
-            painter->setPen(option.palette.text().color());
+    if (option.state & QStyle::State_Selected)
+      painter->setPen(option.palette.highlightedText().color());
+    else
+      painter->setPen(option.palette.text().color());
 
-        QRect titleRect = option.rect;
-        titleRect.setX(titleRect.x()+5);
-        titleRect.setWidth(titleRect.x()-5);
+    QRect titleRect = option.rect;
+    titleRect.setX(titleRect.x()+5);
+    titleRect.setWidth(titleRect.x()-5);
 
-        QFont font=painter->font() ;
-        font.setPointSize(10);
+    QFont font=painter->font() ;
+    font.setPointSize(10);
 
-        // Title
-        font.setWeight(QFont::Bold);
-        painter->setFont(font);
-        painter->drawText(QPoint(option.rect.x()+10, option.rect.y()+23), item->note()->title());
+    // Title
+    font.setWeight(QFont::Bold);
+    painter->setFont(font);
+    painter->drawText(QPoint(option.rect.x()+10, option.rect.y()+23), item->note()->title());
 
-        // Date
-        font.setWeight(QFont::Normal);
-        painter->setFont(font);
-        painter->drawText(QPoint(option.rect.x()+10, option.rect.y()+43), item->note()->dateCreatedStr());
+    // Date
+    font.setWeight(QFont::Normal);
+    painter->setFont(font);
+    painter->drawText(QPoint(option.rect.x()+10, option.rect.y()+43), item->note()->dateCreatedStr());
 
 
-        // The Excerpt
-        QRect excerptRect = option.rect;
-        excerptRect.setX( excerptRect.x()+10 );
-        excerptRect.setY( excerptRect.y()+50 );
-        excerptRect.setWidth( excerptRect.width()-50 );
+    // The Excerpt
+    QRect excerptRect = option.rect;
+    excerptRect.setX( excerptRect.x()+10 );
+    excerptRect.setY( excerptRect.y()+50 );
+    excerptRect.setWidth( excerptRect.width()-50 );
 
-        QString excerpt = item->note()->text();
-        if (excerpt.length() > 50) {
-          excerpt = excerpt.mid(0, 50) + "...";
-        }
-        excerpt.replace("\n", " ");
-        painter->drawText(excerptRect, excerpt);
-
-        painter->fillRect(QRect(
-                              option.rect.x() + option.rect.width() - 60,
-                              option.rect.y(),
-                              60,
-                              option.rect.height()), background);
-
-        QIcon favoriteIcon;
-        if (item->note()->favorited())
-            favoriteIcon = QIcon::fromTheme("vibrato-draw-star-solid");
-        else
-            favoriteIcon = QIcon::fromTheme("vibrato-draw-star");
-
-        favoriteIcon.paint(painter, getStarRect(option));
-
-    } else {
-        QStyledItemDelegate::paint(painter, option, index);
+    QString excerpt = item->note()->text();
+    if (excerpt.length() > 50) {
+      excerpt = excerpt.mid(0, 50) + "...";
     }
+    excerpt.replace("\n", " ");
+    excerpt.replace(QRegExp("\\s\\s+"), " "); // Ensure at most one space
+    painter->drawText(excerptRect, excerpt);
+
+    painter->fillRect(QRect(
+                            option.rect.x() + option.rect.width() - 60,
+                            option.rect.y(),
+                            60,
+                            option.rect.height()), background);
+
+    QIcon favoriteIcon;
+    if (item->note()->favorited())
+      favoriteIcon = QIcon::fromTheme("vibrato-draw-star-solid");
+    else
+      favoriteIcon = QIcon::fromTheme("vibrato-draw-star");
+
+    favoriteIcon.paint(painter, getStarRect(option));
+
+  } else {
+    QStyledItemDelegate::paint(painter, option, index);
+  }
 }
 
 QSize NoteItemDelegate::sizeHint(const QStyleOptionViewItem &option, const QModelIndex &index) const
 {
-    return QSize(200,100);
+  return QSize(200,100);
 }
