5b83a276e52cb29aebaff61e21d555736d2e52a2
Doug Beney <contact@dougie.io>
2019-01-31 14:27:39 -0500

    changes

--- src/cloud/cloudmanager.cpp
+++ src/cloud/cloudmanager.cpp
@@ -4,3 +4,54 @@ CloudManager::CloudManager()
 {
 
 }
+
+QDateTime CloudManager::lastSync() const {
+  return m_lastSync;
+}
+
+QDateTime CloudManager::lastRefresh() const {
+  return m_lastRefresh;
+}
+
+bool CloudManager::noteExists(QUuid syncHash, bool refresh)
+{
+  if (refresh)
+    refreshNotes();
+  return m_noteList.contains(syncHash);
+}
+
+bool CloudManager::notebookExists(QUuid syncHash, bool refresh)
+{
+  if (refresh)
+    refreshNotebooks();
+  return m_notebookList.contains(syncHash);
+}
+
+bool CloudManager::tagExists(QUuid syncHash, bool refresh)
+{
+  if (refresh)
+    refreshTags();
+  return m_tagList.contains(syncHash);
+}
+
+void CloudManager::refresh()
+{
+  refreshNotes();
+  refreshNotebooks();
+  refreshTags();
+}
+
+void CloudManager::refreshNotes()
+{
+
+}
+
+void CloudManager::refreshNotebooks()
+{
+
+}
+
+void CloudManager::refreshTags()
+{
+
+}
--- src/cloud/cloudmanager.h
+++ src/cloud/cloudmanager.h
@@ -1,6 +1,9 @@
 #ifndef CLOUDMANAGER_H
 #define CLOUDMANAGER_H
 #include <QObject>
+#include <QDateTime>
+#include <QVector>
+#include <QUuid>
 #include "api/vibrato-cloud-api.h"
 
 class CloudManager : public QObject
@@ -9,14 +12,25 @@ class CloudManager : public QObject
 public:
   CloudManager();
 
-  int noteExists(QUuid syncHash, bool refresh=false);
-  int notebookExists(QUuid syncHash, bool refresh=false);
-  int tagExists(QUuid syncHash, bool refresh=false);
+  QDateTime lastSync() const;
+  QDateTime lastRefresh() const;
+
+  bool noteExists(QUuid syncHash, bool refresh=false);
+  bool notebookExists(QUuid syncHash, bool refresh=false);
+  bool tagExists(QUuid syncHash, bool refresh=false);
 
   void refresh();
   void refreshNotes();
   void refreshNotebooks();
   void refreshTags();
-};
+
+private:
+  QDateTime m_lastSync;
+  QDateTime m_lastRefresh;
+
+  QVector<QUuid> m_noteList;
+  QVector<QUuid> m_notebookList;
+  QVector<QUuid> m_tagList;
+}
 
 #endif // CLOUDMANAGER_H
